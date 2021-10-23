6f7d8b1e993addd28aaa6a1fd44772d5103df1a0
shohrab11 <shohrabaiub7@gmail.com>
2021-10-24 01:01:58 +0600

    secound commit

--- src/Components/Player/Player.js
+++ src/Components/Player/Player.js
@@ -14,7 +14,7 @@ backgroundColor: '#3399ff'
 }
 
 const Player = (props) => {
-  const {name,age,image }= props.play;
+  const {name,age,image,salary }= props.play;
   const handleAddToClub = props.handleAddToClub;
   
     return (
@@ -22,6 +22,7 @@ const Player = (props) => {
             <img style={imageStyle}  src={image} alt="" />
             <h3>Name: {name}</h3>
             <h3>Age: {age}</h3>
+            <h5>Salary: {salary}</h5>
             <button className="btn btn-success"  onClick={()=> handleAddToClub(props.play)} ><FontAwesomeIcon icon={faUserPlus} />Add to Team</button>
         </div>
     );
